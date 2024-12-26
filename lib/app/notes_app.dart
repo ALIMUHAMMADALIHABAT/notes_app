a40c1c16d3743e864bcdecb10eb77661872e49af
ALIMUHAMMADALIHABAT <habatali64@gmail.com>
2024-12-26 22:47:08 +0300

    init ScreenUtilInit

--- lib/app/notes_app.dart
+++ lib/app/notes_app.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+     return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_ , child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'First Method',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: child,
+        );
+      },
+     // child: const HomePage(title: 'First Method'),
+    );
   }
 }
